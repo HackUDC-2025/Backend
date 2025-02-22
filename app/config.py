19fd7e639d329b731de7aee2b6e46478b3306012
AlejandroDopico2 <alejandro.dopico2@udc.es>
2025-02-22 11:20:11 +0100

    Change dataset path

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MILVUS_HOST: str = "127.0.0.1"
     MILVUS_PORT: int = 19530
-    DATASET_PATH:str  = "dataset"
+    DATASET_PATH:str  = "dataset/art_images"
     MILVUS_COLLECTION_NAME: str = "museum_embeddings"
     PARAMS_SEARCH: dict = {
         "index_type": "IVF_FLAT",
